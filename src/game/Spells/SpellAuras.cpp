0209735118c6688d74462d86308193fa2f8eb95c
Nightingale9002 <143975831+Nightingale9002@users.noreply.github.com>
2026-08-03 10:05:22 +0800

    Revert "修复猫德回能"
    This reverts commit 2e603ad8236c47b327855ca0a61afa8a58098918.

--- src/game/Spells/SpellAuras.cpp
+++ src/game/Spells/SpellAuras.cpp
@@ -3363,7 +3363,7 @@ void Aura::HandleAuraModShapeshift(bool apply, bool Real)
                 if (m_modifier.m_miscvalue == FORM_CAT)
                 {
                     if (target->IsPlayer())
-                        //static_cast<Player*>(target)->UpdateEnergyRegen();
+                        static_cast<Player*>(target)->UpdateEnergyRegen();
                     target->SetPower(POWER_ENERGY, 0);
                     if (irand(1, 100) <= furorChance)
                         target->CastSpell(target, 17099, TRIGGERED_OLD_TRIGGERED, nullptr, this);
